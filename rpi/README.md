0a39125366eb64bcc92e2af6ff6c79837065f5d1
Matt Vertescher <mvertescher@gmail.com>
2020-03-15 16:04:45 -0400

    Basic custom pi-gen build framework

---
+++ rpi/README.md
@@ -0,0 +1,4 @@
+# Raspberry Pi CI Runner OS
+
+This directory uses the official `pi-gen` repository to configure a custom
+Raspbian Lite based OS to become a CI runner.
---
+++ rpi/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Custom Raspberry Pi image creation.
+#
+# This script overlays custom configuration and stages to `pi-gen` and builds
+# in Docker.
+
+set -eux
+
+cd pi-gen
+
+# Reset the submodule
+git checkout . && git clean -dxf
+
+# Add custom stage
+cp -r ../stage-common .
+
+echo "IMG_NAME='runner'
+DEPLOY_ZIP=0
+TARGET_HOSTNAME=rpi
+ENABLE_SSH=1
+STAGE_LIST='stage0 stage1 stage2 stage-common'
+" > config
+
+# Don't create images for the 'lite' build
+touch ./stage2/SKIP_IMAGES
+
+# Actually run pi-gen
+# TODO: Add clean option: docker rm -v pigen_work && ./build-docker.sh
+PRESERVE_CONTAINER=1 CONTINUE=1 ./build-docker.sh
+
+# Clean up
+rm -rf stage-common
