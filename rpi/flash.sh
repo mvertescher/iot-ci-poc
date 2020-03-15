09fd7ac6951ea41d97e41ef24da3932e243fb17c
Matt Vertescher <mvertescher@gmail.com>
2020-03-15 16:25:40 -0400

    Basic flashing script

---
+++ rpi/flash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Flash a raw `pi-gen` image to an sdcard (mmcblk0 by default).
+
+set -eu
+
+: "${DEVICE:=/dev/mmcblk0}"
+
+IMG=$(ls ./pi-gen/deploy/*talos.img | head -1)
+
+echo "Flashing " $IMG " ..."
+sudo dd bs=4M if=$IMG of=$DEVICE conv=fsync
+echo "Done!"
