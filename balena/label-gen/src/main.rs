b9cdcc856caf514742d911f986101fa6de497b4b
Matt Vertescher <mvertescher@gmail.com>
2020-03-30 00:02:30 -0400

    Cargo fmt

--- balena/label-gen/src/main.rs
+++ balena/label-gen/src/main.rs
@@ -47,8 +47,7 @@ async fn main() {
 
     // TODO: Add config path option
     let config_path = "./config.toml";
-    let config = std::fs::read_to_string(config_path)
-        .expect("Failed to read from config file.");
+    let config = std::fs::read_to_string(config_path).expect("Failed to read from config file.");
     let config: Config = toml::from_str(&config).unwrap();
 
     let mut labels = HashMap::new();
