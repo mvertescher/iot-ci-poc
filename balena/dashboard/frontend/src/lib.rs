390a7dd7db47475560e44ded30fd0674d992e4b5
Matt Vertescher <mvertescher@gmail.com>
2020-04-06 21:58:13 -0400

    Watch support; Websocket handshake working

--- balena/dashboard/frontend/src/lib.rs
+++ balena/dashboard/frontend/src/lib.rs
@@ -153,7 +153,7 @@ impl Component for Model {
                     });
                     let task = self
                         .ws_service
-                        .connect("ws://localhost:9001/", callback, notification)
+                        .connect("ws://localhost:8080/ws/", callback, notification)
                         .unwrap();
                     self.ws = Some(task);
                 }
