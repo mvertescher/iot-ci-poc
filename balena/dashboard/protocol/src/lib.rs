ceb954a1b0915c1b4b7912cca17d466c56733319
Matt Vertescher <mvertescher@gmail.com>
2020-04-10 19:06:18 -0400

    Protocol log message constructor

--- balena/dashboard/protocol/src/lib.rs
+++ balena/dashboard/protocol/src/lib.rs
@@ -3,12 +3,18 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-enum Message {
-    Log
+pub enum Message {
+    Log(Log)
+}
+
+impl Message {
+    pub fn log(data: String) -> Message {
+        Message::Log(Log { data })
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Log {
+pub struct Log {
     data: String,
 }
 
